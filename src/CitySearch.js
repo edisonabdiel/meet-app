6cf9cd03cab1d23fa4ee90d221e97c506b99dc8a
Edison Abdiel <edison.abdiel@soundcloud.com>
2021-07-13 12:30:24 +0200

    feat: Add integration tests for App.js and CitySesrch.js. Add token check middlewear to api.js and App.js

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -2,48 +2,53 @@ import React, { Component } from 'react';
 
 class CitySearch extends Component {
 
-    state = {
-        query: '',
-        suggestions: []
-    }
+  state = {
+    query: '',
+    suggestions: [],
+    showSuggestions: undefined
+  }
 
-    handleInputChanged = (event) => {
-        const value = event.target.value;
-        const suggestions = this.props.locations.filter((location) => {
-          return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
-        });
-        this.setState({
-          query: value,
-          suggestions,
-        });
-      };
+  handleInputChanged = (event) => {
+    const value = event.target.value;
+    const suggestions = this.props.locations.filter((location) => {
+      return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
+    });
+    this.setState({
+      query: value,
+      suggestions,
+    });
+  };
 
-      handleItemClicked = (suggestion) => {
-        this.setState({
-          query: suggestion
-        });
-      }
+  handleItemClicked = (suggestion) => {
+    this.setState({
+      query: suggestion,
+      showSuggestions: false
+    });
 
-    render() {
-        return (
-                <div className="citySearch" >
-                    <input type="text"
-                        className="city"
-                        label="Search for events by City"
-                        value={this.state.query}
-                        onChange={this.handleInputChanged} />
-                    <ul className="suggestions">
-                        {this.state.suggestions.map((suggestion) => (
-                            <li key={suggestion}
-                                onClick={() => this.handleItemClicked(suggestion)}>{suggestion}</li>
-                        ))}
-                        <li key='all'>
-                            <b>See all cities</b>
-                        </li>
-                    </ul>
-                </div>
-        );
-    }
+    this.props.updateEvents(suggestion);
+  }
+
+  render() {
+    return (
+      <div className="citySearch" >
+        <input type="text"
+          className="city"
+          label="Search for events by City"
+          value={this.state.query}
+          onChange={this.handleInputChanged}
+          onFocus={() => { this.setState({ showSuggestions: true }) }} />
+        <ul className="suggestions" style={this.state.showSuggestions ? {}: { display: 'none' }}>
+          {this.state.suggestions.map((suggestion) => (
+            <li key={suggestion}
+              onClick={() => this.handleItemClicked(suggestion)}>{suggestion}</li>
+          ))}
+          <li key='all' onClick={() => this.handleItemClicked("all")}>
+            <b>See all cities</b>
+          </li>
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default CitySearch;
