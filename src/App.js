b4e88cd7b41514ae0dd7bc8c1871a932f195b09c
Edison Abdiel <edison.abdiel@soundcloud.com>
2021-07-19 08:40:36 +0200

    fix: Add numberOfEvents as props to CitySearch.

--- src/App.js
+++ src/App.js
@@ -71,9 +71,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
-        <EventList events={this.state.events} />
+        <h1 className="app-name">Meet App</h1>
+        <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} numberOfEvents={this.numberOfEvents} />
         <NumberOfEvents updateEvents={this.updateEvents} numberOfEvents={this.state.numberOfEvents} />
+        <EventList events={this.state.events} /> 
       </div>
     );
   }
--- src/EventList.js
+++ src/EventList.js
@@ -1,11 +1,10 @@
+// React modules
 import React, { Component } from 'react';
-
 //Custom Components
 import Event from './Event';
 
 export default class EventList extends Component {
     render() {
-
         const { events } = this.props;
         return (
             <>
